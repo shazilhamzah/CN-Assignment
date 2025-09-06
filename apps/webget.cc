07107ebfbfe414f2231c7ecc2b355006da9f12c0
Ubuntu <shazilvm@example.com>
2025-09-06 16:53:14 +0000

    Implemented get_URL function in apps/webget.cc

--- apps/webget.cc
+++ apps/webget.cc
@@ -9,9 +9,18 @@ using namespace std;
 
 void get_URL( const string& host, const string& path )
 {
-  cerr << "Function called: get_URL(" << host << ", " << path << ")\n";
-  cerr << "Warning: get_URL() has not been implemented yet.\n";
+	TCPSocket socket;
+	Address address(host,"http");
+	socket.connect(address);
+	const string request = "GET "+path+" HTTP/1.1\r\nHost: "+host+"\r\nConnection: close\r\n\r\n";
+	socket.write(request);
+	string buffer;
+	while(!socket.eof()){
+		socket.read(buffer);
+		cout<<buffer;
+	}
 }
+
 
 int main( int argc, char* argv[] )
 {
